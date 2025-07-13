57a90af86bf38d8f91b365f94a9a837920e0c427
Daniel Diaz <diazjfdaniel@gmail.com>
2025-07-12 20:43:23 -0700

    fix: styling of textbox and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { GeminiProvider } from "./llm_provider";
 import { createClient, TABLES } from "./utils";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const promptFactory = (ruleset: string[], text: string) => {
   return `
@@ -8,33 +8,41 @@ const promptFactory = (ruleset: string[], text: string) => {
     You MUST adhere to the following rules and never deviate from them to successfully complete the task.
     
     Rules to follow:
-    ${ruleset.map((rule) => `- ${rule}`).join('\n')}
+    ${ruleset.map((rule) => `- ${rule}`).join("\n")}
 
     Text to edit:
     ${text}
-  `
-}
+  `;
+};
 
 const mapOptionsToRuleset = (options: string[]) => {
   const map = {
-    'conciseness': 'Be concise and to the point. Remove any unnecessary words or phrases but do not remove any information necessary to communicate the full intent of the text.',
-    'grammar': 'Ensure the text is grammatically correct and follows the rules of the English language.',
-    'style': 'Ensure the text is written in a style that is consistent with the intended audience.',
-    'formatting': 'Ensure the text is formatted correctly and follows the rules of the English language.',
-    'punctuation': 'Ensure the text is punctuated correctly and follows the rules of the English language.',
-    'spelling': 'Ensure the text is spelled correctly and follows the rules of the English language.',
-  }
+    conciseness:
+      "Be concise and to the point. Remove any unnecessary words or phrases but do not remove any information necessary to communicate the full intent of the text.",
+    grammar:
+      "Ensure the text is grammatically correct and follows the rules of the English language.",
+    style:
+      "Ensure the text is written in a style that is consistent with the intended audience.",
+    formatting:
+      "Ensure the text is formatted correctly and follows the rules of the English language.",
+    punctuation:
+      "Ensure the text is punctuated correctly and follows the rules of the English language.",
+    spelling:
+      "Ensure the text is spelled correctly and follows the rules of the English language.",
+  };
 
-  return options.map((option) => map[option as keyof typeof map]).filter(Boolean);
-}
+  return options
+    .map((option) => map[option as keyof typeof map])
+    .filter(Boolean);
+};
 
 export async function submitText(formData: FormData) {
-  'use server';
+  "use server";
 
   const db = await createClient();
 
-  const text = formData.get('text') as string;
-  const rawOptions = (formData.get('options') as string).split(',');
+  const text = formData.get("text") as string;
+  const rawOptions = (formData.get("options") as string).split(",");
   const options = rawOptions.map((option) => option.trim());
 
   // TODO: Validate the text and options
@@ -43,29 +51,47 @@ export async function submitText(formData: FormData) {
   const prompt = promptFactory(ruleset, text);
 
   const provider = new GeminiProvider();
-  const response = await provider.generateText(prompt, 'gemini-2.5-flash');
+  const response = await provider.generateText(prompt, "gemini-2.5-flash");
 
   const { error } = await db.from(TABLES.SUBMISSIONS).insert({
     original_text: text,
     selected_options: options,
     prompt,
     response,
-  })
+  });
 
   if (error) {
-    throw new Error(`Unable to save submission: ${error.message}`)
+    throw new Error(`Unable to save submission: ${error.message}`);
   }
 
   // TODO: Revalidate cache.
   // Redirect to the submission page.
 }
 
-
 export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+    <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center w-full">
+        <Tabs
+          defaultValue="form"
+          className="w-full flex flex-col items-center gap-4"
+        >
+          <TabsList className="">
+            <TabsTrigger value="form">Form</TabsTrigger>
+            <TabsTrigger value="submissions">Submissions</TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="form"
+            className="w-full flex flex-col items-center gap-4"
+          >
+            <div className="w-full">
+              <TextArea />
+            </div>
+          </TabsContent>
+          <TabsContent value="submissions">
+            <Submissions />
+          </TabsContent>
+        </Tabs>
       </main>
     </div>
   );
@@ -74,8 +100,18 @@ export default async function Home() {
 function TextArea() {
   return (
     <form action={submitText}>
-      <textarea name="text" placeholder="Enter the piece of text to edit" />
-      <button type="submit">Submit</button>
+      <div className="flex flex-col gap-4 items-center">
+        <textarea
+          name="text"
+          placeholder="Enter the piece of text to edit"
+          className="border border-gray-300 rounded-md p-2 w-3/5 h-[350px]"
+        />
+        <button type="submit">Submit</button>
+      </div>
     </form>
-  )
+  );
+}
+
+function Submissions() {
+  return null;
 }
